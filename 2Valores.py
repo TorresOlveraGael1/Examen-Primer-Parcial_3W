b8f820fb945b60c99c9c986af4578e8961731533
TorresOlveraGael1 <a.23308051281218@cbtis128.edu.mx>
2024-09-26 15:49:26 -0600

    Add files via upload

---
+++ 2Valores.py
@@ -0,0 +1,23 @@
+#Desarrollar código que lea dos valores que captures, determinar cual de los dos valores es el menor
+
+print (" ")
+print ("Torres Olvera Gael")
+print (" ")
+
+
+A = int(input("Introduzca el primer valor: ")) #Establecemos los
+B = int(input("Introduzca el segundo valor: ")) #Valores a utilizar
+
+print (" ") #Salto de linea
+
+#Abrimos cadena 'If'
+if A > B:
+    print (A, "Es mayor que: ", B)
+else:
+    print (B, "Es mayor que:", A)
+
+print (" ") #Salto de linea
+
+suma = A+B #Ordena al sistema realizar una operacion
+
+print ("La suma de ambos valores es: ", suma) #Ordena al sistema mostrar el resultado
---
+++ Perimetro.py
@@ -0,0 +1,12 @@
+#Hallar el perímetro y el área de un rectángulo
+
+print (" ")
+print ("Torres Olvera Gael")
+print (" ")
+
+A = int(input("Introduzca el primer valor: ")) #Establecemos los
+B = int(input("Introduzca el segundo valor: ")) #Valores a utilizar
+
+perimetro = A*2 + B*2 #Establece la operacion a realizar
+
+print ("El perimetro del rectangulo es: ", perimetro) #Ordena ala sistema a mostrar el resultado de la operacion
