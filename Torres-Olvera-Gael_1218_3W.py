39539d8c978471d8f4bcbfb2fcd1fe69b67c12a5
TorresOlveraGael1 <a.23308051281218@cbtis128.edu.mx>
2024-09-26 14:42:49 -0600

    Add files via upload

---
+++ Torres-Olvera-Gael_1218_3W.py
@@ -0,0 +1,23 @@
+#Desplegar tu nombre completo con los últimos 4 dígitos de tu numero de control y el grupo al inicio de cada ejercicio
+
+print (" ")
+print ("Torres Olvera Gael")
+print (" ")
+
+Ap = ("Torres") #Establecemos los
+Am = ("Olvera") #Valores del
+Na = ("Gael") #Nombre por separado
+
+print (Ap) #Imprimimos por
+print (Am) #Separado los
+print(Na) #Valores establecidos
+
+print (" ") #Salto de linea
+
+print ("Tu nombre es: ", Ap, Am, Na) #Imprime el nombre
+
+print (" ") #Salto de linea
+
+Name = (Ap + " " + Am + " " + Na) #Simplifica la linea de codigo
+
+print (Name.upper()) #Establece el comando upper para imprimirlo en mayusculas
